838ea3f66a0f5f3248eff228e45c22a24db8494c
derleymad <70535956+derleymad@users.noreply.github.com>
2022-06-08 22:52:00 -0300

    percent of a day added (implementing button calculator)

--- app/src/main/java/me/dio/cripto_info_app/MainActivity.kt
+++ app/src/main/java/me/dio/cripto_info_app/MainActivity.kt
@@ -1,22 +1,43 @@
 package me.dio.cripto_info_app
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import org.json.JSONObject
 import java.net.URL
+import java.util.*
 import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     private lateinit var resultBTC: TextView
     private lateinit var resultETH: TextView
+    private lateinit var sinalBTC: TextView
+    private lateinit var sinalETH: TextView
+    private lateinit var converterBTC: TextView
+    private lateinit var inputBTC: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         resultBTC = findViewById<TextView>(R.id.priceBTC)
         resultETH = findViewById<TextView>(R.id.priceETH)
+        sinalBTC = findViewById<TextView>(R.id.sinalBTC)
+        sinalETH = findViewById<TextView>(R.id.sinalETH)
+        converterBTC = findViewById<TextView>(R.id.converterBTC)
+
+        inputBTC = findViewById<EditText>(R.id.inputBTC)
+        var btnBTC = findViewById<Button>(R.id.btnBTC)
+
+        btnBTC.setOnClickListener {
+            calcular()
+        }
         getBTCvalue()
         getETHvalue()
   }
@@ -28,10 +49,31 @@ class MainActivity : AppCompatActivity() {
             try {
                 val dataBTC = conBTC.inputStream.bufferedReader().readText()
                 val objBTC = JSONObject(dataBTC)
+
+                val ticker = objBTC.getJSONObject("ticker")
+
+                val resBTC = ticker.getDouble("last")
+                val lowBTC = ticker.getDouble("low")
+                val highBTC = ticker.getDouble("high")
+                val sinal = highBTC.minus(lowBTC)
+                val total = (sinal * 100) / highBTC
+
                 runOnUiThread {
-                    val resBTC = objBTC.getJSONObject("ticker").getDouble("last")
+                    if (sinal>0){
+                        //BTC ta subindo ++
+                        sinalBTC.visibility = View.VISIBLE
+                        sinalBTC.setTextColor(Color.parseColor("#00FF00"))
+                        sinalBTC.text = "+ ${String.format("%.2f",total)}%"
+
+                    }else{
+                        //BTC ta caindo --
+                        sinalBTC.visibility = View.VISIBLE
+                        sinalBTC.setTextColor(Color.parseColor("#FF0000"))
+                        sinalBTC.text = "${String.format("R$ %.2f",total)}%"
+                    }
+
                     //resultBTC.text = "R$ ${resBTC.toString().format(2)} "
-                    resultBTC.text = "R$ ${resBTC.toString().format(2)}"
+                    resultBTC.text = "R$ ${String.format("%.2f",resBTC)}"
                 }
             }finally {
                 conBTC.disconnect()
@@ -40,6 +82,7 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun getETHvalue(){
+
         Thread{
             val urlETH = URL("https://www.mercadobitcoin.net/api/ETH/ticker") //ETH URL
             val conETH = urlETH.openConnection() as HttpsURLConnection
@@ -48,15 +91,38 @@ class MainActivity : AppCompatActivity() {
                 val dataETH = conETH.inputStream.bufferedReader().readText()
                 val objETH = JSONObject(dataETH)
 
-                runOnUiThread { //for BTC and ETH
+                val ticker = objETH.getJSONObject("ticker")
+
+                val resETH = ticker.getDouble("last")
+                val lowETH = ticker.getDouble("low")
+                val highETH = ticker.getDouble("high")
+                val sinal = highETH.minus(lowETH)
+                val total = (sinal * 100) / highETH
+
+                runOnUiThread { //ETH
 
                     val resETH = objETH.getJSONObject("ticker").getDouble("last")
-                    resultETH.text = "R$ ${resETH.toString().format(2)}"
+                    resultETH.text = "R$ ${String.format("%.2f",resETH)}"
+
+                    if (total>0){
+                        sinalETH.visibility = View.VISIBLE
+                        sinalETH.setTextColor(Color.parseColor("#00FF00"))
+                        sinalETH.text = "+${String.format("%.2f",total)}%"
+                    }else{
+                        sinalETH.visibility = View.VISIBLE
+                        sinalETH.setTextColor(Color.parseColor("#FF0000"))
+                        sinalETH.text = "+${String.format("%.2f",total)}%"
+                    }
                 }
             }finally {
                 conETH.disconnect()
             }
         }.start()
 
+    }
+    private fun calcular(){
+       val num = inputBTC.text.toString().toDouble()
+       val total = num.div(resultBTC.text.toString().toDouble())
+        converterBTC.text = total.toString()
     }
 }
